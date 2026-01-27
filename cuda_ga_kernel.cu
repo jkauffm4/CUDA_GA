4072dd4479dc7f3cff23fd4b06314d5ca9b8531b
jkauffm4 <jkauffm4@charlotte.edu>
2026-01-27 18:07:47 -0500

    Refactor fitness function and add selection methods

--- cuda_ga_kernel.cu
+++ cuda_ga_kernel.cu
@@ -18,5 +18,44 @@ __global__ void fitnessfunc(float* pop, float* fit, float* ax, float* ay, float*
             err += diff * diff;
         }
         fit[i] = err;
+        //fit[i] = 1.0f / (1.0f + err);
+    }
+}
+
+__global__ void tournamentSelect(float* pop, float* fit, float* parents, curandState* states, int P, int k) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < P) {
+        int best = -1;
+        float bestFit = 1e30f;
+
+        for (int j = 0; j < k; j++) {
+            int r = curand(&states[i]) % P;
+            if (fit[r] < bestFit) {
+                bestFit = fit[r];
+                best = r;
+            }
+        }
+        parents[i*3+0] = pop[best*3+0];
+        parents[i*3+1] = pop[best*3+1];
+        parents[i*3+2] = pop[best*3+2];
+    }
+}
+
+__global__ void crossover(float* parents, float* children, curandState* states, int P) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < P) {
+
+        // pick two random parents
+        int p1 = curand(&states[i]) % P;
+        int p2 = curand(&states[i]) % P;
+
+        float alpha = curand_uniform(&states[i]); // in (0,1]
+
+        // genes: A, x, y
+        for (int g = 0; g < 3; g++) {
+            float a = parents[p1*3 + g];
+            float b = parents[p2*3 + g];
+            children[i*3 + g] = alpha * a + (1.0f - alpha) * b;
+        }
     }
 }
